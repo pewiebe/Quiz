1166b07d3b47ba92c18c8c0c789d8eb68445effd
Samuel <118542381+pewiebe@users.noreply.github.com>
2023-01-02 22:24:43 -0300

    commit

---
+++ questions.js
@@ -0,0 +1,84 @@
+let questions = [
+    {
+        numb: 1,
+        question: "O que aconteceria se o sol fosse substituído por um buraco negro de mesma massa?",
+        answer: "Não ia acontecer nada",
+        options: [
+            "Não iamos sobreviver",
+            "Não ia acontecer nada",
+            "O sistema solar não ia existir",
+            "Nenhuma das opções estão corretas"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Quantos anos Getúlio Vargas ficou no poder?",
+        answer: "Quinze anos",
+        options: [
+            "Quinze anos",
+            "Quatro anos",
+            "Não ganhou nenhuma eleição",
+            "Apenas oito anos",
+        ]
+    },
+    {
+        numb: 3,
+        question: "Em que continente está localizada a Índia?",
+        answer: "Ásia",
+        options: [
+            "Ásia",
+            "Oriente Médio",
+            "América do Sul",
+            "Continente Europeu",
+        ]
+    },
+    {
+        numb: 4,
+        question: "Quantos copas do mundo o Brasil tem?",
+        answer: "Cinco copas",
+        options: [
+            "Cinco copas",
+            "Duas copas",
+            "Nenhuma copa",
+            "Sete copas",
+        ]
+    },
+    {
+        numb: 5,
+        question: "Qual o maior planeta do sistema solar?",
+        answer: "Júpiter",
+        options: [
+            "Sol",
+            "Júpiter",
+            "Saturno",
+            "Neturno",
+        ]
+    },
+
+    {
+        numb: 6,
+        question: "Qual o idioma mais falado no mundo?",
+        answer: "Inglês",
+        options: [
+            "Chinês",
+            "Hindi",
+            "Inglês",
+            "Português",
+        ]
+
+    },
+    // you can uncomment the below codes and make duplicate as more as you want to add question
+    // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+    //   {
+    //   numb: 6,
+    //   question: "Your Question is Here",
+    //   answer: "Correct answer of the question is here",
+    //   options: [
+    //     "Option 1",
+    //     "option 2",
+    //     "option 3",
+    //     "option 4"
+    //   ]
+    // },
+];
